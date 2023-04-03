af4dbb81c7c0fb59221e47d1239234a1176368ac
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-04-03 16:48:32 -0300

    initializing repository

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log('server running on http://localhost:8080/');
+});
+
+const MONGO_URL = 'mongodb+srv://mateusbrbza:Kr506bsqXPN4AX2l@rest-api-build.ovkvw3s.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
