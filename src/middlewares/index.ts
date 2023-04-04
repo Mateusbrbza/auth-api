dc2e25e61eb74403eb0f3ff39a3a63a36dad54f9
Mateusbrbza <mateus.c.barboza@gmail.com>
2023-04-03 23:05:38 -0300

    feat: finished users controllers and router. Project finished

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,6 +3,26 @@ import { get, merge } from 'lodash';
 
 import { getUserBySessionToken } from '../db/users';
 
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req, 'identity._id') as string;
+
+        if (!currentUserId) {
+            return res.sendStatus(404);
+        }
+
+        if(currentUserId.toString() === id) {
+            return res.sendStatus(404);
+        }
+
+        next();
+    } catch (err) {
+        console.log(err);
+        return res.sendStatus(400);
+    }
+}
+
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
         const sessionToken = req.cookies['MATEUS-AUTH'];
